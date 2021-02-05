35849da70d4b19232da58c483f72c71482a41b1c
Jsiddle <jaredsiddle@gmail.com>
2021-02-04 21:56:51 -0500

    Inheritance Problem

--- Inheritance/Program.cs
+++ Inheritance/Program.cs
@@ -6,20 +6,6 @@ namespace Inheritance
     {
         static void Main(string[] args)
         {
-            // TODO Be sure to follow best practice when creating your classes
-
-            // Create a class Animal
-            // give this class 4 members that all Animals have in common
-
-
-            // Create a class Bird
-            // give this class 4 members that are specific to Bird
-            // Set this class to inherit from your Animal Class
-
-            // Create a class Reptile
-            // give this class 4 members that are specific to Reptile
-            // Set this class to inherit from your Animal Class
-
 
 
 
@@ -29,11 +15,41 @@ namespace Inheritance
              * Creatively display the class member values 
              */
 
+            var myBird = new Bird();
+
+            myBird.WingColor = "red";
+            myBird.CanFly = true;
+            myBird.DoMigrate = true;
+            myBird.BeakLength = 3.5;
+
+
+
+
             /*Create an object of your Reptile class
              *  give values to your members using the object of your Bird class
              *  
              * Creatively display the class member values 
              */
+
+            var Lizard = new Reptile()
+            {
+                IsColdBlooded = true, IsScaly = true, Habitat = "swamp", CanGrowTail = true
+            };
+
+            var myAnimal = new Animal[] { myBird, Lizard };
+
+            foreach (var Animal in myAnimal)
+            {
+                Console.WriteLine($"Alive: {Animal.IsAlive}");
+                Console.WriteLine($"Age: {Animal.Age} years old");
+                Console.WriteLine($"It has {Animal.LegCount} legs");
+                Console.WriteLine($"It lives by {Animal.LandSeaAir} ");
+                Console.WriteLine($"");
+
+
+
+
+            }
         }
     }
 }
